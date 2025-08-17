c17422627d042af3f5d4e470dcb62a0181858904
AttilaART <amwpinter@icloud.com>
2025-08-17 16:35:59 +0200

    initfile

--- init.sh
+++ init.sh
@@ -4,3 +4,7 @@ echo "Building Project: Attilaart"
 cd "$(dirname "$0")"
 docker build -t attilaart .
 docker run --name attilaart -d -p 8000:8000 attilaart
+ufw allow 8000
+ufw enable
+
+echo "Successfully initialised Attilaart"
