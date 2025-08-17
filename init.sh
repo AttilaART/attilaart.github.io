370ae5c17dc5c9a0d0d06f36bee9055e50b11b88
AttilaART <amwpinter@icloud.com>
2025-08-17 16:44:02 +0200

    init file

--- init.sh
+++ init.sh
@@ -2,16 +2,20 @@
 
 echo "Building Project: Attilaart"
 cd "$(dirname "$0")"
-docker build -t attilaart .
 
 if ! docker rmi attilaart; then
   echo "ERROR: failed to remove previous instance"
   exit 126
-else
-  if ! docker run --name attilaart -d -p 8000:8000 attilaart; then
-    echo "ERROR: failed to start docker container instance"
-    exit 126
-  fi
+fi
+
+if ! docker build -t attilaart .; then
+  echo "ERROR: failed to build docker image"
+  exit 126
+fi
+
+if ! docker run --name attilaart -d -p 8000:8000 attilaart; then
+  echo "ERROR: failed to start docker container instance"
+  exit 126
 fi
 
 ufw allow 8000
